fe8348d379ba4e31b1c477def1dfd7b050369290
AaronEGH16 <aezegonz@gmail.com>
2023-12-20 01:43:50 +0000

    last push

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -9,18 +9,12 @@ if __name__ == "__main__":
     from sys import argv
 
     id = int(argv[1])
-    NUMBER_OF_DONE_TASKS = 0
-    TOTAL_NUMBER_OF_TASKS = 0
     url = f"https://jsonplaceholder.typicode.com/users/{id}"
     EMPLOYEE_NAME = requests.get(url).json()["name"]
     task_record = requests.get(url + "/todos").json()
-    for tasks in task_record:
-        TOTAL_NUMBER_OF_TASKS += 1
-        if tasks["completed"]:
-            NUMBER_OF_DONE_TASKS += 1
+    task_completed = [task for task in task_record if task["completed"]]
 
-    print(f"Employee {EMPLOYEE_NAME} is done" +
-          f" with tasks({NUMBER_OF_DONE_TASKS}/{TOTAL_NUMBER_OF_TASKS}):")
-    for task in task_record:
-        if task['completed']:
-            print(f"\t {task['title']}")
+    print(f"Employee {EMPLOYEE_NAME} is done", end="")
+    print(f" with tasks({len(task_completed)}/{len(task_record)}):")
+    for task in task_completed:
+        print(f"\t {task['title']}")
