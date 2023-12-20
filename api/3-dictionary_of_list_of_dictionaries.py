153190c7667b3d1cb0ab705eaf5269a36e080b93
AaronEGH16 <aezegonz@gmail.com>
2023-12-20 21:17:41 +0000

    dict of all users and tasks

---
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+    this module contains a simple API gether
+    and json exporter
+"""
+
+
+if __name__ == "__main__":
+    """main function"""
+    import json
+    import requests
+
+    url = f"https://jsonplaceholder.typicode.com"
+    user_record = requests.get(url + "/users").json()
+    task_record = requests.get(url + "/todos").json()
+
+    json_obj = {
+        f"{user['id']}": [{
+                "task": task["title"],
+                "completed": task["completed"],
+                "username": user["username"]
+                } for task in task_record]
+        for user in user_record
+    }
+
+    with open(f"todo_all_employees.json", "w") as jsonfile:
+        json.dump(json_obj, jsonfile)
