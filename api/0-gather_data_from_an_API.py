29719bb49ba7b13ff0af34fbef42413675ec2a0f
AaronEGH16 <aezegonz@gmail.com>
2023-12-20 01:29:59 +0000

    re writed all code

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,28 +1,31 @@
 #!/usr/bin/python3
 """
-    this module contains a simple API data gether
-
-    get from user and todo lists:
-        - user name
-        - number of tasks
-        - tasks completed
-        - task title
-    and print text
+    this module contains a simple API gether
 """
 
-import requests
-from sys import argv
-
 if __name__ == "__main__":
-    url = f"https://jsonplaceholder.typicode.com/"
+    """
+        main function
+    """
+    import requests
+    from sys import argv
 
-    id = int(argv[1])
-    employee = requests.get(url + f"users/{id}").json()
-    tasks_record = requests.get(url + f"users/{id}/todos").json()
+    id = argv[1]
+    user_id = f"https://jsonplaceholder.typicode.com/users/{id}"
+    todo = f"https://jsonplaceholder.typicode.com/todos/"
+    tasks = 0
+    completed = 0
+    completed_list = []
+    user = requests.get(user_id).json()
+    name = user.get("name")
+    task_record = requests.get(todo).json()
+    for task in task_record:
+        if task.get("userId") == int(id):
+            if task["completed"]:
+                completed_list.append(task["title"])
+                completed += 1
+            tasks += 1
 
-    tasks_completed = [tasks for tasks in tasks_record if tasks["completed"]]
-
-    print(f"Employee {employee['name']} is done with ", end="")
-    print(f"tasks({len(tasks_completed)}/{len(tasks_record)}):")
-    for task in tasks_completed:
-        print(f"\t " + task["title"])
+    print(f"Employee {name} is done with tasks({completed}/{tasks}):")
+    for title in completed_list:
+        print(f"\t {title}")
