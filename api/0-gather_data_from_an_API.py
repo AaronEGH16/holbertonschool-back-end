288c7c444da35b9c8f505d929b1fdf9906a2f79e
AaronEGH16 <aezegonz@gmail.com>
2023-12-20 01:34:16 +0000

    back to original version

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -4,28 +4,23 @@
 """
 
 if __name__ == "__main__":
-    """
-        main function
-    """
+    """main function"""
     import requests
     from sys import argv
 
-    id = argv[1]
-    user_id = f"https://jsonplaceholder.typicode.com/users/{id}"
-    todo = f"https://jsonplaceholder.typicode.com/todos/"
-    tasks = 0
-    completed = 0
-    completed_list = []
-    user = requests.get(user_id).json()
-    name = user.get("name")
-    task_record = requests.get(todo).json()
-    for task in task_record:
-        if task.get("userId") == int(id):
-            if task["completed"]:
-                completed_list.append(task["title"])
-                completed += 1
-            tasks += 1
+    id = int(argv[1])
+    NUMBER_OF_DONE_TASKS = 0
+    TOTAL_NUMBER_OF_TASKS = 0
+    url = f"https://jsonplaceholder.typicode.com/users/{id}"
+    EMPLOYEE_NAME = requests.get(url).json()["name"]
+    task_record = requests.get(url + "/todos").json()
+    for tasks in task_record:
+        TOTAL_NUMBER_OF_TASKS += 1
+        if tasks["completed"]:
+            NUMBER_OF_DONE_TASKS += 1
 
-    print(f"Employee {name} is done with tasks({completed}/{tasks}):")
-    for title in completed_list:
-        print(f"\t {title}")
+    print(f"Employee {EMPLOYEE_NAME} is done" +
+          f" with tasks({NUMBER_OF_DONE_TASKS}/{TOTAL_NUMBER_OF_TASKS}):")
+    for task in task_record:
+        if task['completed']:
+            print(f"\t {task['title']}")
